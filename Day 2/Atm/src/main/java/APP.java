05def5024a6a11aa6b00aa7b3aa8f2e7fef3271b
JeevithaVijayakumarN <jeevithav0710@gmail.com>
2022-08-12 17:59:51 +0530

    Day 2

---
+++ Day 2/Atm/src/main/java/APP.java
@@ -0,0 +1,5 @@
+public class APP {
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation(9,3,7);
+    }
+}
---
+++ Day 2/Atm/src/main/java/Calculation.java
@@ -0,0 +1,13 @@
+public class Calculation {
+    private int a;
+    private int b;
+    private int c;
+
+
+    public Calculation(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        System.out.println(a*b*c);
+    }
+}
